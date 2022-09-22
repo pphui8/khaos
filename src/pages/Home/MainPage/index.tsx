f99a00113298f92d70142f0e8a0d4369df6dab98
金晖 <55843852+pphui8@users.noreply.github.com>
2022-09-22 17:35:54 +0800

    v0,0,8

--- src/pages/Home/MainPage/index.tsx
+++ src/pages/Home/MainPage/index.tsx
@@ -1,12 +1,66 @@
 import React from "react";
 import { Card, Col, Divider, Row } from "antd";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 import ServerStatus from "./ServerStatus";
 import SumData from "./SumData";
 import "./index.css";
+import { Line } from "react-chartjs-2";
 
 type Props = {};
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  responsive: true,
+  interaction: {
+    mode: "index" as const,
+    intersect: false,
+  },
+  stacked: false,
+  scales: {
+    y: {
+      type: "linear" as const,
+      display: true,
+      position: "left" as const,
+    }
+  },
+};
+
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
 export default function index({}: Props) {
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "最近七天下单数量",
+        data: labels.map((_, index) => {
+          return index * 100;
+        }),
+        borderColor: "#8bdaff",
+        backgroundColor: "#1890ff",
+        yAxisID: "y",
+      },
+    ],
+  };
+  
   return (
     <>
       <Row align="middle">
@@ -21,6 +75,7 @@ export default function index({}: Props) {
       <Row>
         <Col span={23}>
           <Card>
+            <Line options={options} data={data} />
           </Card>
         </Col>
       </Row>
